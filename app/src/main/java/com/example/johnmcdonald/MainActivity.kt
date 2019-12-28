1e3d97a0a703e020290f4cd861b6bb3b4e546af7
win-cyber <tunthandar863@gmail.com>
2019-12-28 09:27:24 -0800

    John

---
+++ app/src/main/java/com/example/johnmcdonald/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.johnmcdonald
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.john_app)
+    }
+}
